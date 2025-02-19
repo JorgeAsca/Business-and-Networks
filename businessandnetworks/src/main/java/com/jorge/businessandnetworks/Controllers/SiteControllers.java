264447d58044d4427839052502e62de2678d4cd0
JorgeAsca <jascola171@g.educaand.es>
2025-02-18 14:23:42 -1100

    Agregando dependencias en el pom de (Mariadb, webjar, materializer)

---
+++ businessandnetworks/src/main/java/com/jorge/businessandnetworks/Controllers/SiteControllers.java
@@ -0,0 +1,9 @@
+package com.jorge.businessandnetworks.Controllers;
+
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SiteControllers {
+
+}
