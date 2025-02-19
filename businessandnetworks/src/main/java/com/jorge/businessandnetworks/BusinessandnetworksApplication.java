4fd5a8d98c4c1dacdc33015915a5df17242a0659
JorgeAsca <jascola171@g.educaand.es>
2025-02-18 13:46:46 -1100

    Iniciando el proyecto de la empresa "Business-and-Networks"

---
+++ businessandnetworks/src/main/java/com/jorge/businessandnetworks/BusinessandnetworksApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.businessandnetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessandnetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessandnetworksApplication.class, args);
+	}
+
+}
